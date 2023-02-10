59fddc171a1a352d150138c0e178f101311651b2
chintan <chintan.tops@gmail.com>
2023-02-10 07:02:14 +0530

    selenium

---
+++ Learning_Corejava/src/basic/B001_First.java
@@ -0,0 +1,17 @@
+package basic;
+
+public class B001_First {
+	public static void main(String[] args) {
+		
+		int a = 10;
+		int b = 20;
+		System.out.println(a);
+		//System.out.println(b);
+		
+		int firstName = 10;
+		int myNameIs = 50;
+		
+		
+		
+	}
+}
